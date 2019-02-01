c271f2e0863fb50811be355e226aabeac44d6e0e
Szymon Guzik <szymon198198@gmail.com>
2019-02-01 14:18:44 +0100

    Basic config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,50 @@
+const path = require('path');
+
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
-  ],
-}
+	siteMetadata: {
+		title: `nowajaga.pl`,
+		description: `Nowajaga.pl - restauracja, pyszne jedzenie, na dowóz, catering i kręgle a wszystko to w jednym miejscu!`,
+		author: `@ghornon`
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `img`,
+				path: `${__dirname}/src/assets/img`
+			}
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `nowajaga.pl`,
+				short_name: `nowajaga.pl`,
+				start_url: `/`,
+				background_color: `#424953`,
+				theme_color: `#424953`,
+				display: `minimal-ui`,
+				icon: `src/assets/img/gatsby-icon.png` // This path is relative to the root of the site.
+			}
+		},
+		`gatsby-plugin-sass`,
+		`gatsby-plugin-postcss`,
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.app/offline
+		'gatsby-plugin-offline',
+		{
+			resolve: `gatsby-plugin-alias-imports`,
+			options: {
+				alias: {
+					Components: path.resolve(__dirname, 'src/components'),
+					Assets: path.resolve(__dirname, 'src/assets'),
+					Pages: path.resolve(__dirname, 'src/pages'),
+					Sass: path.resolve(__dirname, 'src/assets/css')
+				},
+				extensions: []
+			}
+		}
+	]
+};
