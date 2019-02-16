0ad781596b2cc36cdeea467a7866f75b43fe1972
Szymon Guzik <szymon198198@gmail.com>
2019-02-16 12:10:11 +0100

    asset improvements

--- src/components/Gallery/GalleryThumbnail/GalleryThumbnail.js
+++ src/components/Gallery/GalleryThumbnail/GalleryThumbnail.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Link, withPrefix } from 'gatsby';
+import { withPrefix } from 'gatsby';
 import { FaSearchPlus } from 'react-icons/fa';
 
 const GalleryThumbnail = ({ imgSrc, primary }) => (
 	<div className={`gallery__grid-item gallery__grid-item--${primary ? 'primary' : 'secondary'}`}>
 		<img src={withPrefix(imgSrc)} alt={imgSrc} className="gallery__image" />
-		<Link to={withPrefix(imgSrc)} className="gallery__link">
-			<div className="gallery__icon">
-				<FaSearchPlus className="gallery__svg" />
-			</div>
-		</Link>
 	</div>
 );
 
